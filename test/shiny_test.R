072a5e1014974c78a136384ea9a9d8ba44d8f0d2
AliciaSchep <aschep@gmail.com>
2017-05-26 15:26:19 -0700

    shiny example!

---
+++ test/shiny_test.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(ggplot2)
+
+# Define the UI
+ui <- fluidPage(
+    plotOutput('plot')
+)
+
+
+# Define the server code
+server <- function(input, output) {
+
+    values <- reactiveValues(x = NULL, y = NULL)
+
+    observeEvent(invalidateLater(100), {
+
+        new_response <- ws_read_one(cc, 0)
+        if (new_response != ""){
+            new_point <- fromJSON(new_response)
+            values$x <- c(values$x, new_point$x)
+            values$y <- c(values$y, new_point$y)
+        }
+    }, ignoreNULL = FALSE)
+
+    output$plot <- renderPlot({
+        ggplot(data.frame(xval = values$x, yval = values$y),
+               aes(x = xval, y=yval)) + geom_point()
+    })
+}
+
+# Return a Shiny app object
+shinyApp(ui = ui, server = server)
